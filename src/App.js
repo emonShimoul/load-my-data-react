58254c11b7b96ca2d26e69bcb67b208f8138ba93
emonShimoul <emonshimoul@gmail.com>
2022-06-23 00:31:56 +0600

    set fetch with post method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
 
   useEffect(() => {
     fetch('http://localhost:5000/users')
     .then(res => res.json())
     .then(data => setUsers(data))
   }, []);
+
+  const handleAddUser = e => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+
+    const newUser = {name: name, email: email}
+
+    // send data to the server
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
       <h2>Users: {users.length}</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" ref={nameRef} placeholder='Name' />
+        <input type="email" ref={emailRef} placeholder='Email' />
+        <input type="submit" value='Submit' />
+      </form>
       <ul>
         {
           users.map(user => <li key={user.id}>{user.id}: {user.name} {user.email}</li>)
